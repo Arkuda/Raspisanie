813fa6388d465f27fb2fc383ea2fd1b5f1d8d15a
Andrey <reyzor2142@gmail.com>
2014-02-02 13:05:21 +0400

    add group selection and local storage for him

--- src/www/js/app.js
+++ src/www/js/app.js
@@ -2,56 +2,94 @@
 
 var app;
 var DayOfWeek = 0;
+var nowGroup = localStorage.getItem("nowGroup");
+var groupsJSON;
+var isOnline;
 
-var nowGroup;
 
 var namesWeek = ["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","ВЫХОДНОЙ НАХ"];
 
 
 function init()
 {
+    checkIsOnline(function(){
+    setAutomDayOfWeek();
     getGroups();
     selectDayOfWeek(getNowDate());
+    });
 }
 
 
 
+
+function checkIsOnline(callback)
+{
+    $.get("http://dl.dropboxusercontent.com/u/61847240/raspisanie/avalible.json",function(data)
+    {
+        console.log("ISONLINE = TRUE;");
+        isOnline = true;
+        callback();
+    }).fail(function(ex) {
+            console.log("ISONLINE = FALSE;");
+            isOnline = false;
+            callback();
+        });
+
+}
 function getGroups()
 {
-    $.getJSON("https://dl.dropboxusercontent.com/u/61847240/raspisanie/avalible.txt",function(data)
-    {
-        var group = eval(data);
-        console.log(group);
-        for(var i = 0; i <= group.size; i++)
+    if(isOnline){
+        $.get("http://dl.dropboxusercontent.com/u/61847240/raspisanie/avalible.json",function(data)
         {
-            addGroup(group[i]);
+            groupsJSON = data;
+            localStorage.setItem("groups-json",data);
+            var _group = JSON.parse(data);
+            for(var i = 0; i <= _group.size-1; i++)
+            {
+                addGroup(_group.groups.split(',')[i]);
+            }
+
+        }).fail(function(ex) {
+                console.error("error of loading avalible.json");
+            });
+    }
+    else
+    {
+        var _group = JSON.parse(localStorage.getItem("groups-json"));
+        for(var i = 0; i <= _group.size-1; i++)
+        {
+            addGroup(_group.groups.split(',')[i]);
         }
-
-    }).fail(function(ex) {
-            console.error("error of loading avalible.json");
-        });
+    }
 }
-
-
 function getNowDate()
 {
     var d = new Date();
     var n = d.getDay();
     return n-1;
 }
-
-
-function addGroup(groupName)
+function addGroup(_groupName)
 {
-    $("#groupsselect").text($("#groupsselect").text() + "<li><a onclick='setNowGroup(" +groupName+ ")\"'>"+groupName+"</a></li>");
-    console.log($("#groupsselect").text() + "<li><a onclick='setNowGroup(" +groupName+ ")\"'>"+groupName+"</a></li>");
+    console.log(_groupName);
+    var ah = $("<a></a>").attr("onclick","setNowGroup(\"" +_groupName + "\")").text(_groupName);
+    var _group = $("<li></li>").append(ah);
+    $("#groupsselct").append(_group);
+    console.log(_group);
 }
-
 function setNowGroup(groupName)
 {
+    localStorage.setItem("nowGroup",groupName);
     nowGroup = groupName;
 }
+function setAutomDayOfWeek()
+{
+    var d = new Date();
+    var n = d.getDay();
 
+    if(n !=6 && n !=0 )selectDayOfWeek(n-1);
+    else selectDayOfWeek(0);
+
+}
 function selectDayOfWeek(i)
 {
     DayOfWeek = i;
